a57674614ad4455a73a21a95b3f1c98c5543c60b
noharms <harms.no@gmail.com>
2022-07-28 19:42:40 +0200

    More readable version of InsertionSort

--- src/main/java/sorting/InsertionSorter.java
+++ src/main/java/sorting/InsertionSorter.java
@@ -23,4 +23,24 @@ public class InsertionSorter<T extends Comparable<T>> implements Sorter<T> {
             ++iterator;
         }
     }
+
+    // alternative, maybe more readable version (only for int)
+//    public static void sortByInsertion(int[] array) {
+//        for (int forwardCounter = 1; forwardCounter <= array.length - 1; ++forwardCounter) {
+//
+//            // "insert" the element array[forwardCounter] to the correct position in the already sorted sub-array left of it;
+//            // do this by repeatedly swapping it to the left if  the left element is larger
+//            for (int backwardCounter = forwardCounter; backwardCounter > 0; --backwardCounter) {
+//                int currentValue = array[backwardCounter];
+//                int leftValue = array[backwardCounter - 1];
+//
+//                if (leftValue <= currentValue) { // this means we have swapped the element often enough, it is "inserted" now
+//                    break;
+//                } else { // leftValue > currentValue --> need to swap
+//                    swap(array, backwardCounter, backwardCounter - 1);
+//                }
+//
+//            }
+//        }
+//    }
 }
