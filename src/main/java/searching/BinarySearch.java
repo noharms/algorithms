fa7e9463da6eb95a47e4dd88a139e9c4cbe43442
noharms <harms.no@gmail.com>
2022-11-07 20:21:38 +0100

    Binary search: lower/upper bound variant

--- src/main/java/searching/BinarySearch.java
+++ src/main/java/searching/BinarySearch.java
@@ -4,6 +4,9 @@ public class BinarySearch {
 
     /**
      * Returns the index of the element that has the specified key, or -1, if no such element is found.
+     * <br>
+     * If the array contains the searched for key multiple times (duplicates), we do not know which of them is found
+     * but only that one of them will be found.
      */
     public static <T extends Comparable<T>> int find(T[] sorted, T key) {
         return findRecursive(sorted, 0, sorted.length, key);
@@ -21,6 +24,70 @@ public class BinarySearch {
             return findRecursive(sorted, midIndex + 1, endExcl, key);
         } else {
             return findRecursive(sorted, startIncl, midIndex, key);
+        }
+    }
+
+
+    /**
+     * The lower bound search differs from {@link BinarySearch#find(Comparable[], Comparable)} in that we expect to
+     * have duplicates in the sorted input array and we are interested in the FIRST index at which a certain key
+     * occurs.
+     * <br>
+     * For example, for input [1, 2, 42, 42, 42, 4, 5] the normal binary search may find index 3 for search key
+     * 42, while the lower bound search will find index 2.
+     * <br>
+     * This comes at the cost of always doing log(n) steps until we have only one element in the search window.
+     * Instead, the normal binary search has a better best case because if it finds the key it just stops.
+     */
+    public static <T extends Comparable<T>> int findLowerBound(T[] sorted, T key) {
+        return findRecursiveLowerBound(sorted, 0, sorted.length, key);
+    }
+
+    private static <T extends Comparable<T>> int findRecursiveLowerBound(T[] sorted,
+                                                                         int startIncl,
+                                                                         int endExcl,
+                                                                         T key) {
+        if (startIncl >= endExcl) {
+            return -1;
+        }
+        int arrayLength = endExcl - startIncl;
+        int midIndex = startIncl + arrayLength / 2;
+        int comparison = key.compareTo(sorted[midIndex]);
+        if (comparison == 0) {
+            int indexHitLeftFromCurrentHit = findRecursiveLowerBound(sorted, startIncl, midIndex, key);
+            return indexHitLeftFromCurrentHit != -1 ? indexHitLeftFromCurrentHit : midIndex;
+        } else if (comparison > 0) {
+            return findRecursiveLowerBound(sorted, midIndex + 1, endExcl, key);
+        } else {
+            return findRecursiveLowerBound(sorted, startIncl, midIndex, key);
+        }
+    }
+
+    /**
+     * See documentation on {@link BinarySearch#findLowerBound(Comparable[], Comparable)}
+     */
+    public static <T extends Comparable<T>> int findUpperBound(T[] sorted, T key) {
+        return findRecursiveUpperBound(sorted, 0, sorted.length, key);
+    }
+
+    private static <T extends Comparable<T>> int findRecursiveUpperBound(T[] sorted,
+                                                                         int startIncl,
+                                                                         int endExcl,
+                                                                         T key) {
+        if (startIncl >= endExcl) {
+            return -1;
+        }
+        int arrayLength = endExcl - startIncl;
+        int midIndex = startIncl + arrayLength / 2;
+        int comparison = key.compareTo(sorted[midIndex]);
+        if (comparison == 0) {
+            int indexHitRightFromCurrentHit = findRecursiveUpperBound(sorted, midIndex + 1, endExcl, key);
+            return indexHitRightFromCurrentHit != -1 ? indexHitRightFromCurrentHit : midIndex;
+
+        } else if (comparison > 0) {
+            return findRecursiveUpperBound(sorted, midIndex + 1, endExcl, key);
+        } else {
+            return findRecursiveUpperBound(sorted, startIncl, midIndex, key);
         }
     }
 }
--- src/test/java/searching/BinarySearchTest.java
+++ src/test/java/searching/BinarySearchTest.java
@@ -10,35 +10,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class BinarySearchTest {
 
     @Test
-    void empty_array_returns_minus1() {
+    void find_on_empty_array_returns_minus1() {
         assertEquals(-1, BinarySearch.find(new Integer[0], 42));
     }
 
     @Test
-    void not_found_returns_minus1() {
+    void find_not_found_returns_minus1() {
         assertEquals(-1, BinarySearch.find(new Integer[]{1}, 42));
     }
 
     @Test
-    void one_element_matches_returns_0() {
+    void find_on_one_element_which_matches_returns_0() {
         assertEquals(0, BinarySearch.find(new Integer[]{42}, 42));
     }
 
     @Test
-    void two_element_matches() {
+    void find_on_two_elements_where_one_matches() {
         assertEquals(0, BinarySearch.find(new Integer[]{42, 43}, 42));
         assertEquals(1, BinarySearch.find(new Integer[]{41, 42}, 42));
     }
 
     @Test
-    void three_element_matches() {
+    void find_on_three_elements_where_one_matches() {
         assertEquals(0, BinarySearch.find(new Integer[]{42, 43, 45}, 42));
         assertEquals(1, BinarySearch.find(new Integer[]{41, 42, 44}, 42));
         assertEquals(2, BinarySearch.find(new Integer[]{40, 41, 42}, 42));
     }
 
     @Test
-    void many_element_matches() {
+    void find_on_many_elements_where_one_matches() {
         Integer[] arr = new Random().ints(100).boxed().toArray(Integer[]::new);
         int someValue = arr[0];
         Arrays.sort(arr);
@@ -46,9 +46,134 @@ class BinarySearchTest {
     }
 
     @Test
-    void strings_work() {
+    void find_on_strings_work() {
         assertEquals(0, BinarySearch.find(new String[]{"42", "43", "45"}, "42"));
         assertEquals(1, BinarySearch.find(new String[]{"41", "42", "44"}, "42"));
         assertEquals(2, BinarySearch.find(new String[]{"40", "41", "42"}, "42"));
+    }
+
+    @Test
+    void find_on_array_with_duplicates_works() {
+        assertEquals(5, BinarySearch.find(new Integer[]{41, 41, 42, 42, 42, 42, 42, 42, 42, 43}, 42));
+    }
+
+    //---------------
+    @Test
+    void findLowerBound_on_empty_array_returns_minus1() {
+        assertEquals(-1, BinarySearch.findLowerBound(new Integer[0], 42));
+    }
+
+    @Test
+    void findLowerBound_on_not_found_returns_minus1() {
+        assertEquals(-1, BinarySearch.findLowerBound(new Integer[]{1}, 42));
+    }
+
+    @Test
+    void findLowerBound_on_one_element_matches_returns_0() {
+        assertEquals(0, BinarySearch.findLowerBound(new Integer[]{42}, 42));
+    }
+
+    @Test
+    void findLowerBound_on_two_elements_where_one_matchs() {
+        assertEquals(1, BinarySearch.findLowerBound(new Integer[]{41, 42}, 42));
+    }
+
+    @Test
+    void findLowerBound_on_two_elements_where_both_match() {
+        assertEquals(0, BinarySearch.findLowerBound(new Integer[]{42, 42}, 42));
+    }
+
+    @Test
+    void findLowerBound_on_three_element_where_one_matches() {
+        assertEquals(0, BinarySearch.findLowerBound(new Integer[]{42, 43, 45}, 42));
+        assertEquals(1, BinarySearch.findLowerBound(new Integer[]{41, 42, 44}, 42));
+        assertEquals(2, BinarySearch.findLowerBound(new Integer[]{40, 41, 42}, 42));
+    }
+
+    @Test
+    void findLowerBound_on_three_element_where_multiple_match() {
+        assertEquals(0, BinarySearch.findLowerBound(new Integer[]{42, 42, 42}, 42));
+        assertEquals(0, BinarySearch.findLowerBound(new Integer[]{42, 42, 45}, 42));
+        assertEquals(1, BinarySearch.findLowerBound(new Integer[]{41, 42, 42}, 42));
+    }
+
+    @Test
+    void findLowerBound_on_four_elements_where_multiple_match() {
+        assertEquals(0, BinarySearch.findLowerBound(new Integer[]{42, 42, 42, 42}, 42));
+        assertEquals(0, BinarySearch.findLowerBound(new Integer[]{42, 42, 42, 45}, 42));
+        assertEquals(0, BinarySearch.findLowerBound(new Integer[]{42, 42, 43, 45}, 42));
+        assertEquals(1, BinarySearch.findLowerBound(new Integer[]{41, 42, 42, 42}, 42));
+        assertEquals(2, BinarySearch.findLowerBound(new Integer[]{41, 41, 42, 42}, 42));
+    }
+
+    @Test
+    void findLowerBound_on_strings_work() {
+        assertEquals(0, BinarySearch.findLowerBound(new String[]{"42", "43", "45"}, "42"));
+        assertEquals(1, BinarySearch.findLowerBound(new String[]{"41", "42", "44"}, "42"));
+        assertEquals(2, BinarySearch.findLowerBound(new String[]{"40", "41", "42"}, "42"));
+
+        assertEquals(0, BinarySearch.findLowerBound(new String[]{"42", "42", "43", "45"}, "42"));
+        assertEquals(1, BinarySearch.findLowerBound(new String[]{"41", "42", "42", "42"}, "42"));
+        assertEquals(2, BinarySearch.findLowerBound(new String[]{"41", "41", "42", "42"}, "42"));
+    }
+
+    //---------------
+    @Test
+    void findUpperBound_on_empty_array_returns_minus1() {
+        assertEquals(-1, BinarySearch.findUpperBound(new Integer[0], 42));
+    }
+
+    @Test
+    void findUpperBound_on_not_found_returns_minus1() {
+        assertEquals(-1, BinarySearch.findUpperBound(new Integer[]{1}, 42));
+    }
+
+    @Test
+    void findUpperBound_on_one_element_matches_returns_0() {
+        assertEquals(0, BinarySearch.findUpperBound(new Integer[]{42}, 42));
+    }
+
+    @Test
+    void findUpperBound_on_two_elements_where_one_matchs() {
+        assertEquals(1, BinarySearch.findUpperBound(new Integer[]{41, 42}, 42));
+    }
+
+    @Test
+    void findUpperBound_on_two_elements_where_both_match() {
+        assertEquals(1, BinarySearch.findUpperBound(new Integer[]{42, 42}, 42));
+    }
+
+    @Test
+    void findUpperBound_on_three_element_where_one_matches() {
+        assertEquals(0, BinarySearch.findUpperBound(new Integer[]{42, 43, 45}, 42));
+        assertEquals(1, BinarySearch.findUpperBound(new Integer[]{41, 42, 44}, 42));
+        assertEquals(2, BinarySearch.findUpperBound(new Integer[]{40, 41, 42}, 42));
+    }
+
+    @Test
+    void findUpperBound_on_three_element_where_multiple_match() {
+        assertEquals(2, BinarySearch.findUpperBound(new Integer[]{42, 42, 42}, 42));
+        assertEquals(1, BinarySearch.findUpperBound(new Integer[]{42, 42, 45}, 42));
+        assertEquals(2, BinarySearch.findUpperBound(new Integer[]{41, 42, 42}, 42));
+    }
+
+    @Test
+    void findUpperBound_on_four_elements_where_multiple_match() {
+        assertEquals(3, BinarySearch.findUpperBound(new Integer[]{42, 42, 42, 42}, 42));
+        assertEquals(2, BinarySearch.findUpperBound(new Integer[]{42, 42, 42, 45}, 42));
+        assertEquals(1, BinarySearch.findUpperBound(new Integer[]{42, 42, 43, 45}, 42));
+        assertEquals(3, BinarySearch.findUpperBound(new Integer[]{41, 42, 42, 42}, 42));
+        assertEquals(3, BinarySearch.findUpperBound(new Integer[]{41, 41, 42, 42}, 42));
+    }
+
+    @Test
+    void findUpperBound_on_strings_work() {
+        assertEquals(0, BinarySearch.findUpperBound(new String[]{"42", "43", "45"}, "42"));
+        assertEquals(1, BinarySearch.findUpperBound(new String[]{"41", "42", "44"}, "42"));
+        assertEquals(2, BinarySearch.findUpperBound(new String[]{"40", "41", "42"}, "42"));
+
+        assertEquals(1, BinarySearch.findUpperBound(new String[]{"42", "42", "43", "45"}, "42"));
+        assertEquals(3, BinarySearch.findUpperBound(new String[]{"41", "42", "42", "42"}, "42"));
+        assertEquals(3, BinarySearch.findUpperBound(new String[]{"41", "41", "42", "42"}, "42"));
     }
 }
